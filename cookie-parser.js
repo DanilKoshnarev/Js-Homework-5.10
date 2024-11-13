e8007f252a28b6bead4063d7f5a53ac799fba4fc
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2024-11-13 12:58:04 +0200

    Update cookie-parser.js

--- cookie-parser.js
+++ cookie-parser.js
@@ -6,26 +6,9 @@ const postRoutes = require('./routes/postRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 
-app.use('/auth', authRoutes);
-app.use('/post', postRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes');
-const postRoutes = require('./routes/postRoutes');
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/auth', authRoutes);
+app.use('/auth', cookieParser(), authRoutes);
 app.use('/post', postRoutes);
 
 const PORT = process.env.PORT || 3000;
